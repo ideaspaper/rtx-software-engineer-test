39ffb450eced91ca8abe6ec056414cabdd791d6a
ideaspaper <agni.infinity@gmail.com>
2022-04-15 09:32:38 +0700

    add cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import router from './routes/index.js';
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
